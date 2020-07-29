36547056d2b39b5f4278c0cdd6a05586c375edca
muhammadfaisal <faisalmfaisal015@gmail.com>
2020-07-29 09:09:06 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './Components/NavBar';
+import Header from './Components/Header';
+import ContentUX from './Components/ContentUX';
+import ContextCopyWrite from './Components/ContextCopyWrite';
+import ContentWriting from './Components/ContentWriting';
+import About from './Components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Header />
+      <ContentUX />
+      <ContextCopyWrite />
+      <ContentWriting />
+      <About />
+    </>
   );
 }
 
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  makeStyles,
+  withStyles,
+  Divider,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  List,
+  Box,
+} from "@material-ui/core";
+import MobileLeftMenuSlider from "@material-ui/core/Drawer";
+
+import { AssignmentInd, Home, Apps, ContactMail } from "@material-ui/icons";
+
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    background:
+      "linear-gradient(90deg, rgba(75,6,193,1) 0%, rgba(206,2,189,1) 100%)",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  topBarText: {
+    width: "500px",
+    margin: "auto",
+    height: "60px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: "22px",
+    color: "white",
+    cursor: "pointer",
+    position: "relative",
+    flexFlow: "column",
+  },
+  sideBarContainer: {
+    width: 250,
+    background:
+      "linear-gradient(90deg, rgba(75,6,193,1) 0%, rgba(206,2,189,1) 100%)",
+    height: "100%",
+  },
+  sideBarHeading: {
+    margin: "1.2rem",
+    textAlign: "center",
+    color: "#fff",
+  },
+  listItem: {
+    color: "#fff",
+  },
+  divider: {
+    background: "#fff",
+  },
+}));
+
+const genericListIcons = [
+  {
+    listIcon: <Home />,
+    listText: "Home",
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contact Me",
+  },
+];
+
+const NavBar = () => {
+  const [visible, setVisible] = useState({
+    left: false,
+  });
+
+  const toggleSlider = (slider, open) => () => {
+    setVisible({ ...visible, [slider]: open });
+  };
+
+  const classes = useStyles();
+
+  const sideBar = () => (
+    <Box
+      className={classes.sideBarContainer}
+      component="div"
+      onClick={toggleSlider("left", false)}
+    >
+      <Typography className={classes.sideBarHeading}>Menu</Typography>
+      <Divider className={classes.divider} />
+      <List>
+        {genericListIcons.map((item, key) => (
+          <ListItem button key={key}>
+            <ListItemIcon className={classes.listItem}>
+              {item.listIcon}
+            </ListItemIcon>
+            <ListItemText
+              className={classes.listItem}
+              primary={item.listText}
+            />
+            <hr />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  return (
+    <div>
+      <AppBar component="nav" position="fixed" className={classes.root}>
+        <Toolbar variant="dense">
+          <IconButton
+            onClick={toggleSlider("left", true)}
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon className={classes.menuIcon} />
+          </IconButton>
+          <Typography
+            variant="h6"
+            color="inherit"
+            className={classes.topBarText}
+          >
+            <span>Muhammad Faisal</span>
+          </Typography>
+          <MobileLeftMenuSlider
+            anchor="left"
+            open={visible.left}
+            onClose={toggleSlider("left", false)}
+          >
+            {sideBar()}
+          </MobileLeftMenuSlider>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default withStyles(useStyles)(NavBar);
